c92d8495c4e436b339670a48773877d09a5f8681
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-11-27 06:44:49 +0300

    solutions for typing_challenges level_1

--- level_1/8.py
+++ level_1/8.py
@@ -1,12 +1,11 @@
-import decimal
 import uuid
 
 from constants import ___
 
 
-def get_user_balance(user_id: ___) -> ___:
+def get_user_balance(user_id: uuid.uuid4) -> float:
     pass
 
 
 if __name__ == "__main__":
-    assert get_user_balance(user_id=uuid.uuid4()) == decimal.Decimal("265.2")
+    assert get_user_balance(user_id=uuid.uuid4()) == float("265.2")
