15a8cda143f88cfcb5e28d2c7f83577903cfaca8
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-11-28 21:32:12 +0300

    fix some bugs

--- level_1/1.py
+++ level_1/1.py
@@ -7,4 +7,3 @@ def is_user_banned(user_id: int) -> bool:
 
 if __name__ == "__main__":
     assert is_user_banned(user_id=32) is True
-
--- level_1/7.py
+++ level_1/7.py
@@ -6,4 +6,12 @@ def send_email(header: str, text_content: str, send_to: str) -> bool:
 
 
 if __name__ == "__main__":
-    assert send_email(header="Test email", text_content="This is a test email", send_to="test@gmail.com") is None
+    assert (
+        send_email(
+            header="Test email",
+            text_content="This is a test email",
+            send_to="test@gmail.com",
+        )
+        is None
+    )
+s
